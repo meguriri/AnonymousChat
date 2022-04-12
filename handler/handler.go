da2d8f8e7b9fb3fe40799c23178254fdcb0613f9
meguriri <1310500986@qq.com>
2022-04-12 17:43:12 +0800

    添加了handler,router

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,54 @@
 package handler
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/meguriri/AnonymousChat/dao"
+	"net/http"
+)
+
+func GetIndex(c *gin.Context) {
+	sid, err := c.Cookie("login")
+	if err != nil {
+		fmt.Println("no cookie")
+		c.HTML(http.StatusOK, "index.html", nil)
+	} else {
+		//dao.CheckSession(s)
+		var s dao.Session
+		sname, err := s.Get(sid)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(sname)
+		//
+	}
+}
+
+func GetOnlineUsers(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"onlineusers": 1000,
+	})
+}
+
+func Login(c *gin.Context) {
+	var user dao.User
+	c.ShouldBindJSON(&user)
+	fmt.Println(user)
+	//var s dao.Session
+	//sid, err := s.Set(user.Nickname, 1e11)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//c.SetCookie("login", sid, int(s.MaxLifetime/1e9), "/", "localhost", false, false)
+
+	//a :=map[string]interface{}{}
+	//a["name"]=name
+	//a["sex"]="male"
+	//err:=redis.Rdb.HMSet(name,a).Err()
+	//if err!=nil{
+	//	fmt.Println("redis err: ",err.Error() )
+	//}
+	c.JSON(http.StatusOK, gin.H{
+		"msg": "ok",
+	})
+}
--- router/router.go
+++ router/router.go
@@ -1,1 +1,16 @@
 package router
+
+import (
+	"github.com/gin-gonic/gin"
+	h "github.com/meguriri/AnonymousChat/handler"
+)
+
+func InitRouter() *gin.Engine {
+	r := gin.Default()
+	r.Static("/static", "./static/")
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/", h.GetIndex)
+	r.GET("/onlineusers", h.GetOnlineUsers)
+	r.POST("/login", h.Login)
+	return r
+}
