c48235bc1c3b812a5a4c8d3f2c0e527795f56b10
meguriri <1310500986@qq.com>
2022-04-12 22:49:40 +0800

    添加了配置信息

--- config/config.go
+++ config/config.go
@@ -2,7 +2,9 @@ package config
 
 import (
 	"fmt"
+	"github.com/meguriri/AnonymousChat/dao"
 	"github.com/meguriri/AnonymousChat/redis"
+	"github.com/meguriri/AnonymousChat/router"
 	"github.com/spf13/viper"
 )
 
@@ -10,12 +12,18 @@ func Configinit() error {
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath("./config")
+
 	if err := viper.ReadInConfig(); err != nil {
 		return err
 	}
 	redis.Address = viper.GetString("redis.address")
 	redis.Password = viper.GetString("redis.password")
 	redis.DB = viper.GetInt("DB")
-	fmt.Println(redis.Address, redis.Password, redis.DB)
+	router.HostAddress = viper.GetString("server.router.hostaddress")
+	router.Port = viper.GetString("server.router.port")
+	dao.MaxUser = viper.GetInt("server.maxuser")
+	dao.MaxLifetime = viper.GetInt64("session.maxlifetime")
+
+	fmt.Println(dao.MaxLifetime)
 	return nil
 }
