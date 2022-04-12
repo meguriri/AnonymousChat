0a3ebd2f3740ff2381ef4a02ce395d10332b33ab
meguriri <1310500986@qq.com>
2022-04-12 17:47:47 +0800

    添加了登录界面内容

--- static/js/login.js
+++ static/js/login.js
@@ -1,12 +1,38 @@
 $(document).ready(function () {
-    $('#enter').click(function (){
+    $('#submitcolor').click(function(){
+        let c1=$('#c1').val()
+        let c2=$('#c2').val()
+        let c3=$('#c3').val()
+        $('#123').css("background-color", "rgb("+String(c1)+","+String(c2)+","+String(c3)+")")
+    })
+    $('#onlineusers').each(function (){
+        $.ajax({
+            type: 'get',
+            url: '/onlineusers',
+            success: function (res) {
+                console.log(res.onlineusers)
+                $('#onlineusers').text(String(res.onlineusers))
+                //window.location.href="/room"
+            }
+        })
+    })
+    $('#submit').click(function (){
         console.log("click..")
-        let a =$('#user').val()
+        let user ={
+            nickname: $('#nicknameinput').val(),
+            gender: Number($("input[type='radio']:checked").val()),
+            color: [
+                parseInt($('#c1').val()),
+                parseInt($('#c2').val()),
+                parseInt($('#c3').val())
+            ]
+        }
+        let json=JSON.stringify(user)
         $.ajax({
             type: 'post',
             url: '/login',
             dataType: 'json',
-            data: {name: a},//序列化表单值
+            data: json,
             success: function (res) {
                 console.log(res.msg)
                 //window.location.href="/room"
