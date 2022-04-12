a0d650b7644b864ddb707360c641bc7edacc05b6
meguriri <1310500986@qq.com>
2022-04-12 17:44:59 +0800

    修改了User数据格式

--- dao/data.go
+++ dao/data.go
@@ -1,7 +1,18 @@
 package dao
 
 type User struct {
-	Nickname string
-	Age      uint
-	Gender   uint
+	//用户昵称
+	Nickname string `json:"nickname"`
+	//用户性别
+	Gender uint `json:"gender"`
+	//头像颜色
+	Color [3]uint `json:"color"`
+}
+
+type Message struct {
+	//头像颜色
+	//性别
+	//昵称
+	//发送时间
+	//内容
 }
